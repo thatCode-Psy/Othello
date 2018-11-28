91db7d4db3d8e43f078395dad422b749a4f22e4b
Jared Okun <okunj20@gmail.com>
2018-11-27 23:08:31 -0500

    Othello game logic is done

--- Assets/BoardScript.cs
+++ Assets/BoardScript.cs
@@ -2,23 +2,203 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BoardSpace {
+    EMPTY,
+    BLACK,
+    WHITE,
+}
+
 public class BoardScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public GameObject piecePrefab;
+    BoardSpace[][] board;
+    GameObject[][] boardGameObjects;
+    uint turnNumber;
+    bool gameStarted;
+
+    List<KeyValuePair<int, int>> currentValidMoves;
+
+    void Awake() {
+        InitBoard();
+
+    }
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetMouseButtonUp(0)) {
             Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            if(clickPosition.y < 4.0 && clickPosition.y > -4.0 && clickPosition.x > -4.0 && clickPosition.x < 4.0) {
+            if (clickPosition.y < 4.0 && clickPosition.y > -4.0 && clickPosition.x > -4.0 && clickPosition.x < 4.0) {
                 int clickX = Mathf.FloorToInt(clickPosition.x) + 4;
                 int clickY = Mathf.FloorToInt(clickPosition.y) + 4;
-                print(clickX + " " + clickY);
+                if (currentValidMoves.Contains(new KeyValuePair<int, int>(clickY, clickX))) {
+                    PlacePiece(clickX, clickY);
+                    
+                }
             }
-            
+
         }
-	}
+
+
+
+    }
+
+    private void InitBoard() {
+        board = new BoardSpace[8][];
+        boardGameObjects = new GameObject[8][];
+        turnNumber = 0;
+        gameStarted = false;
+        for (int i = 0; i < 8; ++i) {
+            board[i] = new BoardSpace[8];
+            boardGameObjects[i] = new GameObject[8];
+            for (int j = 0; j < 8; ++j) {
+                board[i][j] = BoardSpace.EMPTY;
+                boardGameObjects[i][j] = null;
+            }
+        }
+        PlacePiece(3, 3);
+        PlacePiece(3, 4);
+        PlacePiece(4, 4);
+        PlacePiece(4, 3);
+        gameStarted = true;
+        List<KeyValuePair<int, int>> moves = GetValidMoves(board, turnNumber);
+
+        currentValidMoves = moves;
+    }
+
+    public void PlacePiece(int x, int y) {
+        GameObject piece = Instantiate(piecePrefab, transform);
+        SpriteRenderer spriteR = piece.GetComponent<SpriteRenderer>();
+        piece.transform.localPosition = new Vector3((float)x - 3.5f, (float)y - 3.5f, 0f);
+        if (turnNumber % 2 == 0) {
+            spriteR.color = Color.black;
+            board[y][x] = BoardSpace.BLACK;
+        } else {
+            board[y][x] = BoardSpace.WHITE;
+        }
+        boardGameObjects[y][x] = piece;
+        if (gameStarted) {
+            List<KeyValuePair<int, int>> changedSpaces = GetPointsChangedFromMove(board, turnNumber, x, y);
+            foreach(KeyValuePair<int, int> space in changedSpaces) {
+                SpriteRenderer spriteRenderer = boardGameObjects[space.Key][space.Value].GetComponent<SpriteRenderer>();  
+                if (turnNumber % 2 == 0) {
+                    spriteRenderer.color = Color.black;
+                    board[space.Key][space.Value] = BoardSpace.BLACK;
+                } else {
+                    spriteRenderer.color = Color.white;
+                    board[space.Key][space.Value] = BoardSpace.WHITE;
+                }
+            }
+            currentValidMoves = GetValidMoves(board, turnNumber + 1);
+            if(currentValidMoves.Count == 0) {
+                ++turnNumber;
+                currentValidMoves = GetValidMoves(board, turnNumber + 1);
+                if(currentValidMoves.Count == 0) {
+                    GameOver();
+                }
+            }
+        }
+
+        ++turnNumber;
+    }
+
+    public static List<KeyValuePair<int, int>> GetPointsChangedFromMove(BoardSpace[][] board, uint turnNumber, int x, int y) {
+        BoardSpace enemyColor = turnNumber % 2 == 0 ? BoardSpace.WHITE : BoardSpace.BLACK;
+        BoardSpace ourColor = turnNumber % 2 == 0 ? BoardSpace.BLACK : BoardSpace.WHITE;
+        if (board.Length != 8 || board[0].Length != 8 || y < 0 || y >= 8 || x < 0 || x >= 8 || board[y][x] != ourColor) {
+            return null;
+        }
+
+        List<KeyValuePair<int, int>> changedSpaces = new List<KeyValuePair<int, int>>();
+
+        for (int k = -1; k < 2; ++k) {
+            for (int l = -1; l < 2; ++l) {
+                if (!((k == 0 && l == 0) || k + y < 0 || k + y >= 8 || l + x < 0 || l + x >= 8) && board[k + y][l + x] == enemyColor) {
+                    int multiplier = 2;
+                    while (k * multiplier + y >= 0 && k * multiplier + y < 8 && l * multiplier + x >= 0 && l * multiplier + x < 8) {
+                        if (board[k * multiplier + y][l * multiplier + x] == BoardSpace.EMPTY) {
+                            break;
+                        } else if (board[k * multiplier + y][l * multiplier + x] == ourColor) {
+                            for(int i = multiplier - 1; i >= 1; --i) {
+                                changedSpaces.Add(new KeyValuePair<int, int>(k * i + y, l * i + x));
+                            }
+                            break;
+                        }
+                        ++multiplier;
+                    }
+
+                }
+            }
+        }
+
+        return changedSpaces;
+    }
+
+    public static List<KeyValuePair<int, int>> GetValidMoves(BoardSpace[][] board, uint turnNumber) {
+        if(board.Length != 8 || board[0].Length != 8) {
+            return null;
+        }
+        List<KeyValuePair<int, int>> possibleMoves = new List<KeyValuePair<int, int>>();
+
+        for(int i = 0; i < 8; ++i) {
+            for(int j = 0; j < 8; ++j) {
+                if (board[i][j] == BoardSpace.EMPTY) {
+                    BoardSpace enemyColor = turnNumber % 2 == 0 ? BoardSpace.WHITE : BoardSpace.BLACK;
+                    BoardSpace ourColor = turnNumber % 2 == 0 ? BoardSpace.BLACK : BoardSpace.WHITE;
+                    for (int k = -1; k < 2; ++k) {
+                        for (int l = -1; l < 2; ++l) {
+                            if (!((k == 0 && l == 0) || k + i < 0 || k + i >= 8 || l + j < 0 || l + j >= 8) && board[k + i][l + j] == enemyColor) {
+                                int multiplier = 2;
+                                while (k * multiplier + i >= 0 && k * multiplier + i < 8 && l * multiplier + j >= 0 && l * multiplier + j < 8) {
+                                    if (board[k * multiplier + i][l * multiplier + j] == BoardSpace.EMPTY) {
+                                        break;
+                                    } else if (board[k * multiplier + i][l * multiplier + j] == ourColor) {
+                                        possibleMoves.Add(new KeyValuePair<int, int>(i, j));
+                                        k = 2;
+                                        l = 2;
+                                        break;
+                                    }
+                                    ++multiplier;
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    void GameOver() {
+        int blackCount = 0;
+        int whiteCount = 0;
+        foreach(BoardSpace[] row in board) {
+            foreach(BoardSpace space in row) {
+                switch (space) {
+                    case (BoardSpace.BLACK):
+                        blackCount++;
+                        break;
+                    case (BoardSpace.WHITE):
+                        whiteCount++;
+                        break;
+                }
+            }
+        }
+        
+        if(blackCount > whiteCount) {
+            print("Black Wins!");
+        } else if (blackCount < whiteCount) {
+            print("White Wins!");
+        } else {
+            print("Tie");
+        }
+
+    }
+
 }
